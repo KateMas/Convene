9d0980ec7b937b65f0ad6462002966a1f121a119
Ekaterina <e.masyuta@gmail.com>
2015-04-08 17:20:09 +0300

    start tweets

--- client/styles/config.rb
+++ client/styles/config.rb
@@ -6,7 +6,7 @@ require 'bootstrap-sass'
 http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
-images_dir = "images"
+images_dir = "../images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
